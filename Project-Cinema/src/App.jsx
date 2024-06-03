8f5c58566ec36c1b4d4ebcf3455aab6f433fed9c
Eddie Yovanny Millan <eymillan@hotmail.com>
2024-06-03 09:13:53 -0500

    Pelicula Favorita - Tarea 2

---
+++ Project-Cinema/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <div class="title">
+      <h1>Mi película favorita</h1>
+      </div>
+      <div class="image-container">
+        <img src="src\assets\pelicula1.png" alt="Foto pelicula" class="image-reduced"/>
+      </div>
+      <div class="text">
+      <p>
+        Cuenta la historia de un coronel retirado, quien se encuentra a la
+        espera de una pensión, marcado por la muerte de su hijo en una juega
+        de gallos. <b>Género:</b> drama <b>Autor:</b> Gabriel García Márquez <b>Año:</b> 1999
+      </p>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
