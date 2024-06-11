6dc5a60e23640fab2466be57b45cfcc871c9f754
Eddie Yovanny <eymillan@hotmail.com>
2024-06-10 21:52:12 -0500

    Aplicación de filtros Tarea 6

--- Project-Cinema/src/App.jsx
+++ Project-Cinema/src/App.jsx
@@ -1,85 +1,113 @@
-import {useState,useEffect} from 'react';
+import { useState,useEffect } from 'react';
 import './App.css';
 import Card from './components/characterCard';
 import Navbar from './components/Navbar';
 import myImage from './assets/RickAndMorty.png';
 
-/* En la tarea anterior definí la siguiente función, la cual se llama desde App.jsx
-   Sin embargo, adjunto el texto:
-  function RickAndMortyCharacterCard({id}) {
-    const [name, setName] = useState('');
-    const [image, setImage] = useState('');
-    const [gender, setGenre] = useState('');
-    const [status, setStatus] = useState('');
-  
-    useEffect(() => {
-      fetch(`https://rickandmortyapi.com/api/character/${id}`)
-        .then(response => response.json())
-        .then(result => {
-          setName(result.name);
-          setImage(result.image);
-          setGenre(result.gender);
-          setStatus(result.status);
-        });
-    }, [id]);
-
-  return (
-    <div>
-      <CharacterCard 
-        name={name}
-        image={image}
-        gender={gender}
-        status={status}
-      />
-    </div>
-  );
-
-  function App() {
-  return (
-    <div>
-      <h1>Personajes de Rick and Morty</h1>
-      <div>
-        <RickAndMortyCharacterCard id={10} />
-        </div>
-        </div>
-      );
-    }
-  export default App;
-  }*/
 
 function App() {
-
-    const [page,setPage]=useState(1);
-    const [info,setInfo]=useState({});
-    const[character,setCharacter]=useState([]);
-    useEffect(()=>{
-      fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
-      .then((response)=>response.json())
-      .then((data)=>{
-        setCharacter(data.results)
-        setInfo(data.info)
-      })
-    },[page]);
-    console.log(info.pages);
-    const incPage=()=>{
-      setPage(page+1);
-    }
+    const [nameFilter, setNameFilter] = useState('');
+    const [genderFilter, setGenderFilter] = useState('');
+    const [speciesFilter,setSpeciesFilter]=useState('');
+    const [charactersList, setCharactersList] = useState([]);
+    const [totalCharacters, setTotalCharacters] = useState(0);
+    const [page, setPage] = useState(1);
+    const [info, setInfo] = useState({});
+    const [error,setError]=useState('');
+  
+    useEffect(() => {
+      fetchCharacters();
+    }, [page]);
+  
+    const fetchCharacters = () => {
+      const filterParams = new URLSearchParams({
+        name: nameFilter,
+        gender: genderFilter,
+        species: speciesFilter,
+        page: page,
+      });  
+      fetch("https://rickandmortyapi.com/api/character/?" + filterParams.toString())
+        .then((response) => response.json())
+        .then((data) => {
+          if(data.results){
+            const speciesSet = new Set(data.results.map(character => character.species));
+            console.log([...speciesSet]);
+            setCharactersList(data.results);
+            setInfo(data.info);
+            setTotalCharacters(data.info.count);
+            setError('');
+            console.log(data);
+          }else{
+            setCharactersList([]);
+            setInfo({});
+            setTotalCharacters(0);
+            setError('No se encontraron personajes');
+          }        
+        });
+        
+    };
+  
+    const onSearch = () => {
+      setPage(1); 
+      fetchCharacters();      
+    };  
+      
     return (
-      <>
-        <div className='image-container'>
-          <img src={myImage} alt="Descripción de la imagen" class="image"/>
+      <div className='App'>
+         <div className='image-container'>
+          <img src={myImage} alt="Descripción de la imagen" className="image"/>
         </div>
-        <Navbar currentPage={page} setPage={setPage} lastPage={info.pages}/>
-        {/* <button className='button' onClick={incPage}>Pagina {page}</button> */}
-        <div className='container'>          
-          {character.lenght !==0 && character.map((char)=>(
+        <div className="filters">
+          <input
+            type="text"
+            placeholder="Nombre"
+            value={nameFilter}
+            onChange={(e) => setNameFilter(e.target.value)}
+          />
+          <select
+            value={genderFilter}
+            onChange={(e) => setGenderFilter(e.target.value)}
+          >
+            <option value="">Seleccione Género</option>
+            <option value="female">Female</option>
+            <option value="male">Male</option>
+            <option value="genderless">Genderless</option>
+            <option value="unknown">Unknown</option>
+          </select>
+          <select
+            value={speciesFilter}
+            onChange={(e) => setSpeciesFilter(e.target.value)}
+          >
+            <option value="">Seleccione Especie</option>
+            <option value="human">Human</option>
+            <option value="alien">Alien</option>
+            <option value="humanoid">Humanoid</option>
+            <option value="poopybutthole">Poopybutthole</option>
+            <option value="Mythological Creature">Mythological Creature</option>
+            <option value="animal">Animal</option>
+            <option value="disease">Disease</option>
+            <option value="robot">Robot</option>
+            <option value="cronenberg">Cronenberg</option>
+            <option value="unknown">Unknown</option>      
+          </select>
+          <button onClick={onSearch} className="button">Buscar</button>
+        </div>
+        <div className="number">
+          <h4>Personajes hallados: {totalCharacters}</h4>
+          {error && <p className="error">{error}</p>}
+        </div>      
+        <div>
+          {!error&&<Navbar currentPage={page} setPage={setPage} lastPage={info.pages}/>}
+        </div>
+        
+        <div className="character-list">
+          {charactersList.lenght !==0 && charactersList.map((char) => (
             <Card key={char.id} name={char.name} image={char.image}
             alt={char.name} status={char.status} gender={char.gender}/>
-        ))}
-          
-        </div>               
-      </>
+          ))}
+        </div>
+      </div>
     );
   };
-
-export default App;
+  
+  export default App;
